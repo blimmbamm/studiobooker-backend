ba256791be14a93918fee219632eec55e4108bdb
Robin Heinz <robin-heinz@web.de>
2025-10-03 08:29:25 +0200

    read cors origins and port from env variables

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const port = parseInt(configService.get<string>('PORT') || '3001', 10);
+  const dashboardUrl =
+    configService.get<string>('DASHBOARD_URL') || 'http://localhost:4200';
+  const websiteUrl =
+    configService.get<string>('WEBSITE_URL') || 'http://localhost:3000';
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:3000'],
+    origin: [dashboardUrl, websiteUrl],
 
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(port);
 }
 
 bootstrap();
